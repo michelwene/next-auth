8277085a0360b1ced5c0d49a35363d3ae6c8b195
Michel Wene <michelwene@hotmail.com>
2022-04-25 14:15:08 -0300

    feat: validate user permissions in Server

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,4 +1,5 @@
 import { useContext, useEffect } from "react";
+import { Can } from "../components/Can";
 import { AuthContext } from "../contexts/AuthContext";
 import { useCan } from "../hooks/useCan";
 import { setupAPIClient } from "../services/api";
@@ -22,8 +23,9 @@ export default function Dashboard() {
   return (
     <>
       <h1>Dashboard: {user?.email}</h1>
-
-      {userCanSeeMetrics && <h2>Metricas</h2>}
+      <Can permissions={["metrics.list"]}>
+        <h2>Metricas</h2>
+      </Can>
     </>
   );
 }
