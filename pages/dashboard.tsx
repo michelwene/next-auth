9e62ea5de4282d0c0a05a8772c096d90227076f4
Michel Wene <michelwene@hotmail.com>
2022-04-25 14:33:42 -0300

    feat: broadcast logout

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -7,7 +7,7 @@ import { api } from "../services/apiClient";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext);
+  const { user, signOut, isAuthenticated } = useContext(AuthContext);
 
   const userCanSeeMetrics = useCan({
     permissions: ["metrics.list"],
@@ -23,6 +23,7 @@ export default function Dashboard() {
   return (
     <>
       <h1>Dashboard: {user?.email}</h1>
+      <button onClick={signOut}>Sign Out</button>
       <Can permissions={["metrics.list"]}>
         <h2>Metricas</h2>
       </Can>
